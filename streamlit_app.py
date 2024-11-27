aa54004832c273720cb70688ba44b181004e26e3
N Hameleers <niels.hameleers@gmail.com>
2024-11-27 10:35:12 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
+st.title("🎈 MAI new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.read_csv('https://raw.githubusercontent.com/LUCE-Blockchain/Databases-for-teaching/refs/heads/main/Framingham%20Dataset.csv')
+
+st.dataframe(df.SEX.value_counts())
